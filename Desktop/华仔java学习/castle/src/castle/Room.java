a7baa404ce3d736b89e35a3b87ff31c4775d4991
Zoe Liu <107218898+zoeee06@users.noreply.github.com>
2024-04-29 00:17:21 -0400

    Add files via upload

---
+++ Desktop/华仔java学习/castle/src/castle/Room.java
@@ -0,0 +1,36 @@
+package castle;
+
+import java.util.HashMap;
+
+public class Room {
+    private String description;
+    private HashMap<String, Room> exits = new HashMap<String, Room>(); //String: direction
+
+    public Room(String description) 
+    {
+        this.description = description;
+    }
+
+    public void setExit(String dir, Room room) 
+    {
+    	exits.put(dir, room);
+    }
+    
+    public Room getExit(String direction) {
+    	return exits.get(direction);
+    }
+
+    @Override
+    public String toString()
+    {
+        return description;
+    }
+    
+    public String getExitDesc() {
+    	StringBuffer sb = new StringBuffer();
+    	for(String dir:exits.keySet()) {
+    		sb.append(dir+" ");
+    	}
+    	return sb.toString();
+    }
+}
